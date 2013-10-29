d291c06ad3c2ad1fe678b0bdf3c1b5612de976c9
assassinpig <assassinpig@gmail.com>
2013-10-29 08:42:52 -0400

    update ruby client.rb

--- ruby/client.rb
+++ ruby/client.rb
@@ -15,12 +15,18 @@ include Socket::Constants
 
 n=0
 line="get"
-while 1 
-	s = TCPSocket.new '192.168.0.108', 9999 
+#while 1 
+#s = TCPSocket.new '127.0.0.1', 27149 
 	#print "puts \n", n
-	s.puts line
-    line = s.gets 
-	n = n+1
-	s.close
+#s.puts line
+#line = s.gets 
+#n = n+1
+#s.close
 	#break if n==100 
-end
+#end
+
+s = TCPSocket.new '127.0.0.1', 27149 
+s.puts line
+#s.gets line
+#print line
+s.close
