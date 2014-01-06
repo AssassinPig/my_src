e8da2dcfab4820ce8862032c7f7d786948eb2d53
zhufeng <zhufeng@ubuntu.(none)>
2014-01-06 21:18:27 +0800

    2014/1/6

--- c/mysql_test/test_mysql.c
+++ c/mysql_test/test_mysql.c
@@ -10,8 +10,8 @@ int createdb()
 		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
 		exit(1);
 	}
-	if (mysql_real_connect(conn, "localhost", "root", 
-				"root", NULL, 3306, NULL, 0) == NULL) {
+	if (mysql_real_connect(conn, "192.168.0.120", "zhufeng", 
+				"654321", "myblog", 3306, NULL, 0) == NULL) {
 		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
 		exit(1);
 	}
@@ -20,15 +20,56 @@ int createdb()
 		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
 		exit(1);
 	}
+
 	mysql_close(conn);
 
 	return 0;
 }
 
+void querydata()
+{
+	MYSQL *conn;
+	conn = mysql_init(NULL);
+	if (conn == NULL) {
+		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+		exit(1);
+	}
+	if (mysql_real_connect(conn, "192.168.0.120", "zhufeng", 
+				"654321", "myblog", 3306, NULL, 0) == NULL) {
+		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+		exit(1);
+	}
+
+	if (mysql_query(conn, "select * from myblog.user")) {
+		printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+		exit(1);
+	}
+
+        MYSQL_RES *result;
+        MYSQL_ROW row;
+        int num_fields;
+        int i;
+
+        result = mysql_store_result(conn);
+        num_fields = mysql_num_fields(result);
+        while ((row = mysql_fetch_row(result)))
+        {
+            for(i = 0; i < num_fields; i++)
+            {
+                printf("%s ", row[i] ? row[i] : "NULL");
+            }
+            printf("\n");
+        }
+        mysql_free_result(result);
+
+        mysql_close(conn);
+}
+
 int main(int argc, char **argv)
 {
     printf("MySQL client version: %s\n", mysql_get_client_info());
-    createdb();
+    //createdb();
+    querydata();
 
     return 0;
 }
